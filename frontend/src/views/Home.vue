c8f3d299a7744284b3a08580ab840c926819c05e
bubment <medgyesiattila@elektromont.hu>
2021-07-07 15:26:04 +0200

    UPDATE Home.Vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,60 +1,12 @@
 <template>
-  <!-- <div class="home">
-    <h1>Hello there</h1>
-  </div> -->
-  <table class="table" v-if="users.length">
-  <thead>
-    <tr>
-      <th scope="col">Name</th>
-      <th scope="col">Gender</th>
-      <th scope="col">Birth</th>
-    </tr>
-  </thead>
-  <tbody>
-    <template v-for="user in users">
-      <tr>
-        <td>{{user.given_name}} {{user.family_name}}</td>
-        <td>{{user.gender}}</td>
-        <td>{{user.birthdate}}</td>
-      </tr>
-    </template>
-  </tbody>
-</table>
-  <Loader v-if="isLoaderOn"/>
+<h1>Trialday task</h1>
 </template>
 
 <script>
-import Loader from '../components/Loader.vue'
+
 
 export default {
     name: 'Home',
-    components : { Loader },
-    // props:{
-    //     projectsSrc : String
-    // },
-    data(){
-        return{
-          users:[],
-          userMessage:"",
-          isLoaderOn:false
-        }
-    },
-    methods:{
-            test: function(){
-
-            }
-    },
-    mounted(){
-      this.isLoaderOn = true
-      fetch("http://localhost:1337/getComments")
-      .then(res => res.json())
-      .then(result => {
-        console.log(result.data)
-        this.users = result.data
-      })
-      .catch(err => this.userMessage(err))
-      .finally(() => this.isLoaderOn = false);
-    }
 }
 </script>
 
