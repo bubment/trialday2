430b78f96960cecbfbf36b0a26997cc6d787d74d
bubment <medgyesiattila@elektromont.hu>
2021-07-07 15:24:55 +0200

    ADD /getPostDetails request to index.js

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ var connection = mysql.createConnection({
     database : 'trialday2'
   });
 
+  connection.connect();
+
 let app = express();
 
 app.use(express.static('./frontend/dist'));
@@ -19,18 +21,66 @@ app.use(express.json());
 
 
 app.get("/getPosts",(req,res)=>{
-    connection.connect();
-    connection.query('SELECT u.username,p.id,p.title,p.body FROM post p LEFT JOIN user u ON u.id = p.userId', function (error, results, fields) {
+    connection.query('SELECT u.username,p.id,p.title FROM post p LEFT JOIN user u ON u.id = p.userId', function (error, results, fields) {
         if(error){
             res.status(503).json({eventCode:1})
         }else{
             res.status(200).json({posts:results})
         }
-        
       });
-      connection.end();
 })
 
+app.post("/getPostDetails",(req,res)=>{
+    let reqBody = req.body;
+
+    if (reqBody.hasOwnProperty('postId') || parseInt(reqBody.postId) > 0) {
+        let retVal = {
+            postInfo:{},
+            comments:[]
+        };
+
+        let getPostInfo = function(callback){
+            connection.query('SELECT u.username,p.title,p.body FROM post p LEFT JOIN user u ON u.id = p.userId WHERE p.id = ?',[reqBody.postId], function (error, results, fields) {
+                if(error){
+                    callback(err);
+                }else{
+                    retVal.postInfo = results[0] ? results[0] : {};
+                    callback();
+                }
+              });
+        }
+
+        let getComments = function(callback){
+            // SELECT name,body FROM comment WHERE postId = 2
+            connection.query('SELECT name,body FROM comment WHERE postId = ?',[reqBody.postId], function (error, results, fields) {
+                if(error){
+                    callback(err);
+                }else{
+                    retVal.comments = results;
+                    callback();
+                }
+              });
+        }
+
+        async.series(
+            [getPostInfo,getComments],
+            function(err){
+                if(err){
+                    console.log(err)
+                    res.status(500).json({eventCode:1})
+                }else{
+                    res.status(200).json({postDetails:retVal})
+                }
+            }
+        )
+
+
+    }else{
+        res.status(400).json({message:"Missing arguments in request body"})
+    }
+})
+
+app.route('/*').get((req, res) => {res.sendFile(__dirname + '/frontend/dist/index.html')});
 
 app.listen(PORT, ()=>{
     console.log(`App is running on port http://localhost:${PORT}.`)
