3e51ad00a5de89b384a282b861e391d25c6ef338
bubment <medgyesiattila@elektromont.hu>
2021-07-07 13:23:42 +0200

    ADD /getPost request to index.js

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
 const express = require("express");
 const async = require("async");
 const fetch = require('node-fetch');
+const mysql = require('mysql');
 
 const PORT = 1337
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'testuser',
+    password : 'testpassword',
+    database : 'trialday2'
+  });
 
 let app = express();
 
 app.use(express.static('./frontend/dist'));
 app.use(express.json());
 
-app.get("/getComments",(req,res)=>{
-    let resultArray = [];
-    let count = 0;
-    async.whilst(
-        function test(cb) { cb(null, count < 20); },
-        function iter(callback) {
-            fetch("https://phantauth.net/user/")
-            .then(res => res.json())
-            .then(data => {
-                resultArray.push(data)
-                count++;
-                callback(null, count);
-            })
-            .catch(err => callback(err) )
-        },
-        function (err, n) {
-            if (err) {
-                res.status(500).json({message:"Somethings wrong here"})
-            }else{
-                res.status(200).json({ data:resultArray })
-            }
-        }
-    );
 
+app.get("/getPosts",(req,res)=>{
+    connection.connect();
+    connection.query('SELECT u.username,p.id,p.title,p.body FROM post p LEFT JOIN user u ON u.id = p.userId', function (error, results, fields) {
+        if(error){
+            res.status(503).json({eventCode:1})
+        }else{
+            res.status(200).json({posts:results})
+        }
+        
+      });
+      connection.end();
 })
+
 
 app.listen(PORT, ()=>{
     console.log(`App is running on port http://localhost:${PORT}.`)
