414a4a5d27210549dd76b99a46471f27c587c778
bubment <medgyesiattila@elektromont.hu>
2021-07-07 15:26:28 +0200

    ADD Post handling to UI

---
+++ frontend/src/views/posts/Post.vue
@@ -0,0 +1,46 @@
+<template>
+  <table class="table" v-if="posts.length">
+  <thead>
+    <tr>
+      <th scope="col">Username</th>
+      <th scope="col">Post title</th>
+    </tr>
+  </thead>
+  <tbody>
+    <template v-for="post in posts">
+      <tr>
+        <td>{{post.username}}</td>
+        <td><router-link :to="{name : 'PostDetails', params:{ id:post.id }}" class="dropdown-item">{{post.title}}</router-link></td>
+      </tr>
+    </template>
+  </tbody>
+</table>
+  <Loader v-if="isLoaderOn"/>
+</template>
+
+<script>
+import Loader from '../../components/Loader.vue'
+
+export default {
+    name: 'Posts',
+    components : { Loader },
+    data(){
+        return{
+          posts:[],
+          userMessage:"",
+          isLoaderOn:false
+        }
+    },
+
+    mounted(){
+      this.isLoaderOn = true
+      fetch("http://localhost:1337/getPosts")
+      .then(res => res.json())
+      .then(result => {
+        this.posts = result.posts
+      })
+      .catch(err => this.userMessage(err))
+      .finally(() => this.isLoaderOn = false);
+    }
+}
+</script>
---
+++ frontend/src/views/posts/PostDetail.vue
@@ -0,0 +1,74 @@
+<template>
+  <div v-if="postInfo.username">
+      <h1>{{ postInfo.title }}</h1><br>
+      <h3>Written by {{ postInfo.username }}</h3>
+      <p>{{ postInfo.body }}</p>
+      <br><br>
+      <div v-if="comments">
+            <h3>Comments</h3><br>
+            <table class="table">
+            <thead>
+                <tr>
+                <th scope="col">Comment Name</th>
+                <th scope="col">Comment Body</th>
+                </tr>
+            </thead>
+            <tbody>
+                <template v-for="comment in comments">
+                <tr>
+                    <td>{{comment.name}}</td>
+                    <td>{{comment.body}}</td>
+                </tr>
+                </template>
+            </tbody>
+            </table>
+      </div>
+  </div>
+  <div v-else>
+      <h1>The post you are looking for is no longer exists or never existed :(</h1>
+  </div>
+  <Loader v-if="isLoaderOn"/>
+</template>
+
+<script>
+import Loader from '../../components/Loader.vue'
+
+export default {
+    props: ['id'],
+    components : { Loader },
+    data(){
+        return{
+            isLoaderOn:false,
+            postId: this.$route.params.id,
+            postInfo: {},
+            comments: []
+        }
+    },
+    mounted(){
+      this.isLoaderOn = true
+
+        let options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({postId:this.postId}),
+        } 
+
+      fetch("http://localhost:1337/getPostDetails",options)
+      .then(res => res.json())
+      .then(result => {
+        this.postInfo = result.postDetails.postInfo
+        this.comments = result.postDetails.comments
+      })
+      .catch(err => {
+        console.log("Error:",err)
+      })
+      .finally(() => this.isLoaderOn = false);
+    }
+}
+</script>
+
+<style>
+
+</style>
